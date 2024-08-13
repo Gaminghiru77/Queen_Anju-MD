c59ea000d37269c257ebeb8114d05b239874f15c
BOT_RASHMIKA <janith60022006@gmail.com>
2024-08-13 15:36:12 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "",
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW THEIR, QUEEN ANJU IS ALIVE...",
 };
