9b4cede93d3be3621bded2e5678082c8e057ed27
RASHMIKA <101487048+KING-RASHMIKA@users.noreply.github.com>
2024-08-17 09:34:21 +0530

    Update song+videodl.js

--- plugins/song+videodl.js
+++ plugins/song+videodl.js
@@ -38,7 +38,54 @@ let downloadUrl = down.dl_url
 
 //send audio message
 await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"👻POWERD BY GAMING RASH👻"},{quoted:mek})
 
+}catch(e){
+console.log(e)
+  reply('${e}')
+}
+})
+
+====================video_dl=======================
+
+cmd({
+    pattern: "video",
+    desc: "To download videos.",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Please give me a url or title")  
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+    
+    
+let desc = `
+😚QUEEN ANJU YT VIDEO DOWNLOADER😚
+
+Title: ${data.title}
+
+Description: ${data.description}
+
+Time: ${data.timestamp}
+
+Ago: ${data.views}
+
+👻POWERED BY GAMING RASH👻
+`
+
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download video
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+//send video message
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"👻POWERD BY GAMING RASH👻"},{quoted:mek})
 
 }catch(e){
 console.log(e)
