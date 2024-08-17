b1146c917565417f1eb23c94e2b39de5dc929678
RASHMIKA <101487048+KING-RASHMIKA@users.noreply.github.com>
2024-08-17 07:49:43 +0530

    Update song+videodl.js

--- plugins/song+videodl.js
+++ plugins/song+videodl.js
@@ -1,1 +1,45 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+cmd({
+    pattern: "song",
+    desc: "To download songs.",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Please give me a url or title")  
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
 
+let desc = '
+😗 *QUEEN_ANJU SONG DOWNLOADER* 😗
+
+title: ${data.title}
+description: ${data.description}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+POWERD BY QUEEN_ANJU..
+
+'
+
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio message
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
+
+
+}catch(e){
+console.log(e)
+  reply('${e}')
+}
+})
