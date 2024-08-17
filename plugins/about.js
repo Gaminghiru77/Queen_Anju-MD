8a872e686d542adaa4e568e8a4e107a03adcd5ff
RASHMIKA <101487048+KING-RASHMIKA@users.noreply.github.com>
2024-08-17 16:48:59 +0530

    Add files via upload

---
+++ plugins/about.js
@@ -0,0 +1,27 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "about",
+    desc: "To get the bot informations.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let about = ` HELLOW THEIR &mention I AM QUEEN_ANJU WHATSAPP BOT 
+              CREATED BY GAMING RASH(JANITH RASHMIKA)..
+              
+              > *© 𝘘𝘜𝘌𝘌𝘕 𝘈𝘕𝘑𝘜 ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ - ᴍᴅ*
+              > *ɢɪᴛʜᴜʙ :* github.com/Mrrashmika/Queen_Anju-MD
+              
+              THANKS FOR USING QUEEN ANJU WHATSAPP BOT MD`
+return await conn.sendMessage(from,{caption:about},{quoted: mek})
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+
