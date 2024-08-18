b592ae3f2489a667e0e57d79b5d970ffcee79191
RASHMIKA <101487048+KING-RASHMIKA@users.noreply.github.com>
2024-08-18 14:21:30 +0530

    Update song+videodl.js

--- plugins/song+videodl.js
+++ plugins/song+videodl.js
@@ -48,7 +48,7 @@ await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"audio/mpeg",f
 
 }catch(e){
 console.log(e)
-  reply('${e}')
+  reply(`${e}`)
 }
 })
 
@@ -101,6 +101,6 @@ await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fi
 
 }catch(e){
 console.log(e)
-  reply('${e}')
+  reply(`${e}`)
 }
 })
