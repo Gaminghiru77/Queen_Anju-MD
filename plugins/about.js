987946af7cd909e94e9892f55729b0cfc39b6efd
RASHMIKA <101487048+KING-RASHMIKA@users.noreply.github.com>
2024-08-19 17:10:00 +0530

    Update about.js

--- plugins/about.js
+++ plugins/about.js
@@ -7,10 +7,10 @@ cmd({
     category: "main",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber,senderName, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let about = ` HELLOW THEIR ${senderName} I AM QUEEN_ANJU WHATSAPP BOT 
+let about = ` HELLOW THEIR ${senderNumber} I AM QUEEN_ANJU WHATSAPP BOT 
               CREATED BY GAMING RASH(JANITH RASHMIKA)..
               
               > *© 𝘘𝘜𝘌𝘌𝘕 𝘈𝘕𝘑𝘜 ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ - ᴍᴅ*
