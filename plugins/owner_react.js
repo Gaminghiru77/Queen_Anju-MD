ccdc44aa34340651b9f3372182138443af36bd28
RASHMIKA <101487048+KING-RASHMIKA@users.noreply.github.com>
2024-08-21 09:37:33 +0530

    Delete plugins/owner_react.js

--- plugins/owner_react.js
+++
@@ -1,8 +0,0 @@
-const { cmd } = require('../command')
-const config = require('../config')
-const isReact = m.message.reactionMessage ? true : false
-
-if(senderNumber.includes(config.BOT_NUMBER)){
-if(isReact) return
-m.react(config.OWNER_REACT)
-}
