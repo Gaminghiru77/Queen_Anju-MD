1efaaf85007da0f435ea5ae0d62f9eaa657f0417
RASHMIKA <101487048+KING-RASHMIKA@users.noreply.github.com>
2024-08-22 11:59:02 +0530

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -11,7 +11,7 @@ async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender
 try{
 
   if (!args[0]) {
-    return conn.reply(m.chat, '*`INGRESA EL LINK DE FACEBOOK`*', m);
+    return reply(m.chat, '*`INGRESA EL LINK DE FACEBOOK`*', m);
   }
 
   await m.react('🕒');
@@ -19,23 +19,23 @@ try{
   try {
     res = await igdl(args[0]);
   } catch (error) {
-    return conn.reply(m.chat, '*`Error al obtener datos. Verifica el enlace.`*', m);
+    return reply(m.chat, '*`Error al obtener datos. Verifica el enlace.`*', m);
   }
 
   let result = res.data;
   if (!result || result.length === 0) {
-    return conn.reply(m.chat, '*`No se encontraron resultados.`*', m);
+    return reply(m.chat, '*`No se encontraron resultados.`*', m);
   }
 
   let data;
   try {
     data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)");
   } catch (error) {
-    return conn.reply(m.chat, '*`Error al procesar los datos.`*', m);
+    return reply(m.chat, '*`Error al procesar los datos.`*', m);
   }
 
   if (!data) {
-    return conn.reply(m.chat, '*`No se encontró una resolución adecuada.`*', m);
+    return reply(m.chat, '*`No se encontró una resolución adecuada.`*', m);
   }
 
   await m.react('✅');
@@ -45,7 +45,7 @@ try{
   try {
     await conn.sendMessage(m.chat, { video: { url: video }, caption: dev, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m });
   } catch (error) {
-    return conn.reply(m.chat, '*`Error al enviar el video.`*', m);
+    return reply(m.chat, '*`Error al enviar el video.`*', m);
   await m.react('❌');
   }
 }catch(e){
