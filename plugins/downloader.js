2b6fd705549de369d2f412d09d6210f418245a48
RASHMIKA <101487048+KING-RASHMIKA@users.noreply.github.com>
2024-08-22 08:41:20 +0530

    𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝙼𝙳

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -1,34 +1,29 @@
 const {cmd , commands} = require('../command')
 const { fbdl } = require('ruhend-scraper');
-
-      
 cmd({
     pattern: "fb",
     desc: "To download facebook videos.",
-    react: "🎥",
     category: "download",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("Please give me a url or title")  
-const search = await fbdl(q)
-let result = search.data;
-      let data;
-      try {
-         data = result.find(i => i.resolution === "720p (HD)");
-         m.reply(`Data Found!`);       
-      } catch {
-         m.reply(`HD not found switch to SD`);
-         data = result.find(i => i.resolution === "360p (SD)")
-      }
-      let video = data.url      
-      conn.adReply(m.chat, loading, cover, m).then(() => {
-         conn.sendFile(m.chat, video, {
-            caption: `𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊`,
-            quoted: m
-         });
-      });
-   },
-});
+    if(!q) return reply("Please give me a url or title")
+    const search = await fbdl(q)
+    const data = search.videos[0];
+    const url = data.url
     
+//download video
+
+let down = await fg.fbdl(url)
+let downloadUrl = down.dl_url
+
+//send video message
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek});
+
+
+}catch(e){
+console.log(e)
+  reply('${e}')
+}
+})
