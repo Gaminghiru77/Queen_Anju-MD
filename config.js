0f090c94523e8c5644ce8c818e23f8536f3883bf
RASHMIKA <101487048+KING-RASHMIKA@users.noreply.github.com>
2024-08-23 10:05:43 +0530

    𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝙼𝙳

--- config.js
+++ config.js
@@ -29,6 +29,5 @@ MODE: process.env.MODE || "public",
 BOT_NUMBER: process.env.BOT_NUMBER || "94717775628",
 OWNER_REACT: process.env.OWNER_REACT || "🔆",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
 };
 
